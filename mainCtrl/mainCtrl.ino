21c64bd79bf521fb3a786d2921fe91e86123e237
GCC\haywardda1 <d.andrew.hayward@gmail.com>
2017-09-13 12:48:19 -0400

    Add relay to writeValues() Create setLight()

--- mainCtrl/mainCtrl.ino
+++ mainCtrl/mainCtrl.ino
@@ -3,6 +3,10 @@
 int serialRate = 9600;
 char buf[24];
 
+/* Colors */
+char[3] white = {255,255,255};
+char[3] off = {0,0,0};
+
 /* Relay Pins */
 int christmasPin = 22;
 int deskLampPin = 23;
@@ -19,7 +23,7 @@ int closetRedPin;
 int closetGreenPin;
 int closetBluePin;
 
-// Bed Pins 
+// Bed Pins
 int bedRedPin;
 int bedGreenPin;
 int bedBluePin;
@@ -35,9 +39,9 @@ struct RGBStrip sign;
 struct RGBStrip closet;
 struct RGBStrip bed;
 
-bool standinglamp = true;
-bool deskLamp = true;
-bool christmasLights = true;
+bool standingLampOn = true;
+bool deskLampOn = true;
+bool christmasLightsOn = true;
 
 // Sets the given strip to the hex value
 void setColor(struct RGBStrip& strip, char hex[3]){
@@ -46,6 +50,47 @@ void setColor(struct RGBStrip& strip, char hex[3]){
   strip.blue = hex[2];
 }
 
+void setLight(char light, bool state){
+  switch (light) {
+    // Christmas Lights
+    case 0x01:
+      christmasLightsOn = state;
+      break;
+    // Standing lamp
+    case 0x02:
+      standingLampOn = state;
+      break;
+    // Desk lamp
+    case 0x03:
+      deskLampOn = state;
+      break;
+    // Sign
+    case 0x11:
+      if(state){
+        setColor(sign, white);
+      } else {
+        setColor(sign, off);
+      }
+      break;
+    // Closet
+    case 0x12:
+      if(state){
+        setColor(closet, white);
+      } else {
+        setColor(closet, off);
+      }
+      break;
+    // Bed
+      if(state){
+        setColor(bed, white);
+      } else {
+        setColor(bed, off);
+      }
+      break;
+    // Kill all
+    case 0xff:
+  }
+}
 // Attempts to read n bytes from the serila buffer
 void serialReadBytes(n){
   for(int i = 0; i < n; i++){
@@ -56,6 +101,16 @@ void serialReadBytes(n){
 // Process serial command
 void processSerial(char command){
   switch (command){
+    // Breathe LEDS
+    // Fade LEDS
+    // Turn off light
+    // Set LED Color
+    // Turn off light
+    case 0x00:
+      serialReadBytes(1);
+      turnOff(b[1]);
+      break;
+    // Turn on light
     default:
       break;
   }
@@ -67,14 +122,19 @@ void writeValues(){
   analogWrite(signRedPin, sign.red);
   analogWrite(signGreenPin, sign.green);
   analogWrite(signBluePin, sign.blue);
-  
+
   analogWrite(closetRedPin, closet.red);
   analogWrite(closetGreenPin, closet.green);
   analogWrite(closetBluePin, closet.blue);
-  
+
   analogWrite(bedRedPin, bed.red);
   analogWrite(bedGreenPin, bed.green);
   analogWrite(bedBluePin, bed.blue);
+
+  // Set outlet relay
+  digitalWrite(christmasPin, christmasLightsOn);
+  digitalWrite(standingLampPin, standingLampOn);
+  digitalWrite(deskLampPin, deskLampOn);
 }
 
 char b;
@@ -94,5 +154,3 @@ void loop() {
     }
   }
 }
-
-
