207832ab66ed1c94dddd3c3c871076c056412cc0
GCC\haywardda1 <d.andrew.hayward@gmail.com>
2017-09-27 14:28:29 -0400

    Lower command cases Add light set functions

--- lightController.py
+++ lightController.py
@@ -4,26 +4,49 @@ class LightController:
     def __init__(self):
         self.lightmap = dict()
         self.commandmap = dict()
+
+        # Reads local files for serial command mapping
+        _getLightMap()
+        _getCommandMap()
         #self.ser = serial.Serial()
         #self.ser.baudrate = 9600
         #self.ser.port = '/dev/ttyACM0'
         #self.ser.port.open()
 
-    def getLightMap(self):
+    def setLightState(self, light, state):
+        # Build serial command from arguments
+        hexString = ""
+        if(state):
+            hexString += commandmap('on')
+        else:
+            hexString += commandmap('off')
+        hexString += lightmap[light]
+
+        # Cast hex to bytes and send command
+        command = bytes.fromhex(hexString)
+        self.ser.write(command)
+
+    def setStripColor(self, strip, color):
+        # Build serial command from arguments
+        hexString = ""
+        hexString += commandmap['color']
+        hexString += lightmap[strip]
+        hexString += color.strip('# \t\n')
+
+        # Cast hex to bytes and send command
+        command = bytes.fromhex(hexString)
+        self.ser.write(command)
+
+    def _getLightMap(self):
         f = open('lightmap.txt', 'r')
         for line in f:
             name, number = line.strip().split(':')
             self.lightmap[name] = number
 
+        print(self.lightmap)
 
-    def getCommandMap(self):
+    def _getCommandMap(self):
         f = open('commandMapping.txt', 'r')
         for line in f:
             command, number = line.strip().split(':')
             self.commandmap[command] = number
-
-        
-
-light = LightController()
-
-light.getCommandMap()
